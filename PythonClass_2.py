0ec60c98e5f187cd1602ae76230c2111ccc8eaa0
Vaasu devan <vaasuceg.96@gmail.com>
2017-02-08 19:35:51 +0530

    Class2

--- PythonClass_2.py
+++ PythonClass_2.py
@@ -23,4 +23,59 @@
     for implemetation in lists and tuples
     
 '''
+# Print Statement
+print "this is a string.."                               #this is a string..
+print "This number 879 is also a string"                 #This number 879 is also a string
+print "We can print in the","same line also"             #We can print in the same line also
 
+#input and raw_input
+num=input("Enter the number..")                          #Enter the numner..12        num=12
+string=raw_input("Enter the string..")                   #Enter the string..vaasu     string=vaasu
+
+#strings
+a="python is fun"
+print a.upper()                                                #PYTHON IS FUN
+print a.lower()                                                #python is fun
+print a.capitalize()                                           #Python is fun
+print a.count('n')                                             #2
+print a.replace("python","c/c++")                              #c/c++ is fun
+
+#numbers
+a=10
+print a+2                                                      #12
+print a-2                                                      #8
+print a*2                                                      #20
+print a/2                                                      #5
+print a%2                                                      #0
+print float(a)/2                                               #5.0
+
+#Conditionals
+if True:
+     print "something"
+else:
+     print "nothing"                                     #something
+     
+num=10
+if num==20:
+     print "num1"
+elif num==30:
+     print "num2"
+else:
+     print "num3"                                       #num3
+     
+#Lists
+a=[1,2,3]
+print a                                                 #[1,2,3]
+a.append(4)
+print a                                                 #[1,2,3,4]
+print len(a)                                            #4                             
+print max(a)                                            #4
+print min(a)                                            #1
+print sorted(a)                                         #[1,2,3,4]
+
+#for implementation in lists (same output for both examples)
+a=[10,20,30]
+for i in a:
+     print i                                           #10
+                                                       #20
+for i in a:print i                                     #30
