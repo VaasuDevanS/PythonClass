d721e5a97391812718e9ffa8caddf75f89a44f4e
Vaasu devan <vaasuceg.96@gmail.com>
2017-02-08 10:05:31 +0530

    Class2

---
+++ PythonClass_2.py
@@ -0,0 +1,26 @@
+#    0    )
+# 8.2.2017(Wed) STUDENTS QUALITY COUNCIL PYTHON CLASS DAY2
+
+'''
+     Previous Class1 summary.
+     
+     Open Source
+     History of Python
+     
+     a=[1,2,3]
+     length, min, max, a.append(), len, range, sum, a.remove(), Memory Sharing
+     
+
+'''
+
+'''
+    
+    print statement, input, raw_input
+    Strings
+    Numbers
+    Conditionals
+    Lists-Tuples
+    for implemetation in lists and tuples
+    
+'''
+
